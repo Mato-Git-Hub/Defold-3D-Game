9a38fd868c1564e7c45e27df30ce057a51efc297
Mato <sortti.matias@gmail.com>
2026-07-07 09:31:33 +0300

    Fixed camera smoothing oscillation

--- Defold-3D-Game/assets/modules/tween.lua
+++ Defold-3D-Game/assets/modules/tween.lua
@@ -2,30 +2,16 @@ local M = {}
 
 -- Function to move an angle towards given target
 function M.move_towards_angle(angle, target, difference)
-	-- Checking if the angle is already close enough to the target
-	
-	
-	-- Checking the shortest direction
-	if math.abs(target-angle) < math.pi then
-		if angle < target then
-			angle = angle + difference
-		else
-			angle = angle - difference
-		end
-	else
-		if angle < target then
-			angle = angle - difference
-		else
-			angle = angle + difference
-		end
+	-- Calculating the shortest distance between the angles
+	local distance = (target - angle + math.pi) % (2 * math.pi) - math.pi
+
+	-- Checking if the angle is already close enough
+	if math.abs(distance) < difference then
+		return target
 	end
 
-	-- Making sure the angle stays within the range of [0, 2pi]
-	if angle > 2 * math.pi then
-		angle = angle - 2 * math.pi
-	elseif angle < 0 then
-		angle = angle + 2 * math.pi
-	end
+	-- Updating the angle while makinig sure it is in the range of [0, 2pi]
+	angle = (angle + difference * math.abs(distance) / distance) % (2 * math.pi)
 	
 	-- Returning the result
 	return angle
