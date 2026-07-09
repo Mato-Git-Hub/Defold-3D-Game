44ea54ad5092064e9bf381117bcdd0d11a240ae1
Mato <sortti.matias@gmail.com>
2026-07-09 15:28:13 +0300

    Added animation to level completed screen

--- Defold-3D-Game/main/prefabs/camera.go
+++ Defold-3D-Game/main/prefabs/camera.go
@@ -2,13 +2,17 @@ components {
   id: "camera_controller"
   component: "/main/scripts/camera/camera_controller.script"
 }
+components {
+  id: "camera_level_completed_animation"
+  component: "/main/scripts/camera/camera_level_completed_animation.script"
+}
 embedded_components {
   id: "camera"
   type: "camera"
   data: "aspect_ratio: 1.0\n"
   "fov: 0.7854\n"
   "near_z: 0.1\n"
-  "far_z: 100.0\n"
+  "far_z: 1000.0\n"
   "auto_aspect_ratio: 1\n"
   ""
 }
